c175b08bcce3938f270673980ecacca914f5d340
Rafael Santos <rafaelpsantos@gmail.com>
2016-02-29 08:05:26 -0300

    Update ttable.py

--- ttable.py
+++ ttable.py
@@ -17,8 +17,10 @@ without complex ETL implementation.
 import datetime
 import hashlib
 import itertools
+import pandas as pd
 import sqlalchemy
-from sqlalchemy import Integer, Date
+from sqlalchemy import TEXT, Integer, Float, REAL, Date, Index, VARCHAR
+from sqlalchemy.orm import Session
 import luigi
 from luigi.contrib import sqla
 
@@ -45,13 +47,10 @@ class TemporalTable(sqla.CopyToTable):
     """
     enddate = datetime.datetime.strptime('2999-12-31', '%Y-%m-%d').date()
     obsolete_deleted_rows = True
+    chunksize = 5000
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.columns.insert(0, (['id', Integer], {'primary_key': True}))
-        self.columns.append((['SysStartDate', Date], {}))
-        self.columns.append((['SysEndDate', Date], {'index': True}))
 
         self.keycache = dict()
         self.idcache = dict()
@@ -68,7 +67,7 @@ class TemporalTable(sqla.CopyToTable):
         """
         m = hashlib.new(algorithm)
 
-        values = [row[col] for col in set(row.keys())
+        values = [row[col] for col in sorted(row.keys())
                   if col not in ['id', 'SysStartDate', 'SysEndDate']]
         for value in values:
             if not isinstance(value, bytes):
@@ -83,33 +82,19 @@ class TemporalTable(sqla.CopyToTable):
     def _prefill_cache(self, conn):
         """Cache hash and id of rows that are currently valid.
         """
-        s = sqlalchemy.select([self.table_bound]).\
-            where(self.table_bound.c.SysEndDate == self.enddate)
-        rows = conn.execute(s)
+        self._logger.debug('Loading temporal table hash cache')
 
-        for row in rows:
-            searchtuple = self._make_search_tuple(row)
-            self.keycache[searchtuple] = self._hash(row)
-            self.idcache[searchtuple] = row['id']
+        with self.output().engine.begin() as conn:
+                engine = self.output().engine
+                session = Session(engine)
 
-    def _insert(self, conn, ins_rows, table_bound):
-        """This method does the actual insertion of the rows of data given by
-        ins_rows into the database.
+                for row in session.query(
+                    self.table_bound).yield_per(self.chunksize):
+                    row_dict = dict(zip(self.table_bound.columns.keys(), row))
+                    searchtuple = self._make_search_tuple(row_dict)
 
-        Args:
-            conn (sqlalchemy.engine): The sqlalchemy connection object.
-            ins_rows (dict): The dictionary of rows with the keys in the
-                format _<column_name>. For example if you have a table with a
-                column name "property", then the key in the dictionary would
-                be "_property". This format is consistent with the bindparam
-                usage in sqlalchemy.
-            table_bound (sqlalchemy.Table): The object referring to the table
-
-        """
-        bound_cols = dict((c, sqlalchemy.bindparam("_" + c.key))
-                          for c in table_bound.columns if c.key != 'id')
-        sql = table_bound.insert().values(bound_cols)
-        conn.execute(sql, ins_rows)
+                    self.keycache[searchtuple] = self._hash(row_dict)
+                    self.idcache[searchtuple] = row_dict['id']
 
     def _set_as_obsolete(self, conn, ids, table_bound):
         """This method set rows as obsolete updating the `SysEndDate` column of
@@ -129,50 +114,6 @@ class TemporalTable(sqla.CopyToTable):
 
         conn.execute(sql, rows)
 
-    def _log_changes(self, rows, conn):
-        """Lookup input rows in the db table and keep history of changes
-        (insert, delete, update) of everything happened on data rows.
-        """
-        new_rows = list()
-        modified_rows = list()
-        
-        for row in rows:
-            row['SysStartDate'] = self.asof
-            row['SysEndDate'] = self.enddate
-        
-             # Get the newest version and compare to that
-            searchtuple = self._make_search_tuple(row)
-        
-            if searchtuple not in self.idcache:
-                # It is a new member. We add the first version.
-                new_rows.append(row)
-            else:
-                # There is an existing version. Check if the attributes
-                # are identical
-                dim_hash = self.keycache[searchtuple]
-                current_hash = self._hash(row)
-                if dim_hash != current_hash:
-                    # The hash of the rows are different. Add the new version
-                    # of the row to the `modified_rows` list, and the id to the
-                    # `obsolete_ids` list.
-                    modified_rows.append(row)
-                    self.obsolete_ids.append(self.idcache[searchtuple])
-        
-                # Add this key to the `seenkeys` dictionary
-                self.seenkeys[searchtuple] = self.idcache[searchtuple]
-        
-        # Insert new rows
-        if new_rows:
-            self._insert(conn, new_rows, self.table_bound)
-        
-        # Insert a new row version of modified rows
-        if modified_rows:
-            self._insert(conn, modified_rows, self.table_bound)
-        
-        self._logger.debug('Finished inserting {} rows '
-                           'and updating {} rows '
-                           'into SQLAlchemy target'
-                           .format(len(new_rows), len(modified_rows)))
 
     def create_table(self, engine):
         """Override of the `luigi.contrib.sqla.create_table()` with only one
@@ -180,6 +121,10 @@ class TemporalTable(sqla.CopyToTable):
 
         """
         def construct_sqla_columns(columns):
+            columns.insert(0, (['id', Integer], {'primary_key': True}))
+            columns.append((['SysStartDate', Date], {}))
+            columns.append((['SysEndDate', Date], {'index': True}))
+
             retval = [sqlalchemy.Column(*c[0], **c[1]) for c in columns]
             return retval
 
@@ -196,8 +141,20 @@ class TemporalTable(sqla.CopyToTable):
                 try:
                     if not con.dialect.has_table(con, self.table):
                         sqla_columns = construct_sqla_columns(self.columns)
-                        self.table_bound = sqlalchemy.Table(self.table, metadata, *sqla_columns)
-                        metadata.create_all(engine)
+                        self.table_bound = sqlalchemy.Table(self.table,
+                                                            metadata,
+                                                            *sqla_columns)
+
+                        # Create index on natural keys
+                        index_name =\
+                            'ix_{}_{}'.format(self.table_bound.name,
+                                              '_'.join(self.natural_key))
+                        index_columns = [getattr(self.table_bound.c, c)
+                                         for c in self.natural_key]
+                        Index(index_name, *index_columns)
+
+                        # Create table
+                        self.table_bound.create(engine, checkfirst=True)
 
                         # Search for TEXT columns
                         bound_cols = \
@@ -225,26 +182,26 @@ class TemporalTable(sqla.CopyToTable):
         that uses SQLAlchemyTarget instead of LocalTarget inputs.
 
         """
-
-        with self.input().engine.begin() as con:
-            metadata = sqlalchemy.MetaData()
-            engine = self.input().engine
-            table = self.input().target_table
-            table_bound = sqlalchemy.Table(table,
-                                           metadata,
-                                           autoload=True,
-                                           autoload_with=engine)
-
-            result = con.execute(table_bound.select())
-            for row in result:
-                yield row
+        try:
+            with self.input().engine.begin() as conn:
+                metadata = sqlalchemy.MetaData()
+                engine = self.input().engine
+                table = self.input().target_table
+                table_bound = sqlalchemy.Table(table,
+                                               metadata,
+                                               autoload=True,
+                                               autoload_with=engine)
+                session = Session(engine)
+                for row in session.query(table_bound).yield_per(self.chunksize):
+                    yield dict(zip(table_bound.columns.keys(), row))
+        except AttributeError:
+            raise TypeError('Input must be an SQLAlchemyTarget')
 
     def run(self):
         """Lookup and insert/update a version of a temporal table row.
         """
         self._logger.info('Updating temporal table {}: {}'
                           .format(self.table, self.update_id()))
-
         output = self.output()
         engine = output.engine
         self.create_table(engine)
@@ -252,34 +209,97 @@ class TemporalTable(sqla.CopyToTable):
         with engine.begin() as conn:
             self._prefill_cache(conn)
             rows = self.rows()
-            all_rows = [dict(zip((c.key for c in self.table_bound.c
-                                  if c.key not in ['id',
-                                                   'SysStartDate',
-                                                   'SysEndDate']), row))
-                        for row in itertools.islice(rows, self.chunk_size)]
+            self._log_changes(rows, conn)
 
-            while all_rows:
-                self._log_changes(all_rows, conn)
-
-                all_rows = [dict(zip((c.key for c in self.table_bound.c
-                                  if c.key not in ['id',
-                                                   'SysStartDate',
-                                                   'SysEndDate']), row))
-                        for row in itertools.islice(rows, self.chunk_size)]
-
+            # Check if there were deleted rows.
+            # We can only check if there were deleted rows after going
+            # through all input rows.
             if self.obsolete_deleted_rows:
-                # Check if there are deleted rows
                 for key, id in self.idcache.items():
                     if key not in self.seenkeys:
                         self.obsolete_ids.append(id)
                 self._logger.debug('{} rows set as obsolete'
                                    .format(len(self.obsolete_ids)))
 
-                # Update the `SysEndDate` attribute in the old row version and
-                # in the deleted rows in the DB.
-                if self.obsolete_ids:
-                    self._set_as_obsolete(conn, self.obsolete_ids,
-                                          self.table_bound)
+            # Update the `SysEndDate` attribute in the old row version and
+            # in the deleted rows in the DB.
+            if self.obsolete_ids:
+                self._set_as_obsolete(conn, self.obsolete_ids,
+                                        self.table_bound)
 
         output.touch()
         self._logger.info("Finished inserting rows into SQLAlchemy target")
+
+    def _log_changes(self, rows, conn):
+        """Lookup input rows in the db table and keep history of changes
+        (insert, delete, update) of everything happened on data rows.
+
+        """
+        self._logger.debug('Logging changes')
+
+        new_rows = list()
+        modified_rows = list()
+        
+        for row in rows:
+            row['SysStartDate'] = self.asof
+            row['SysEndDate'] = self.enddate
+        
+             # Get the newest version and compare to that
+            searchtuple = self._make_search_tuple(row)
+        
+            if searchtuple not in self.idcache:
+                # It is a new member. We add the first version.
+                new_rows.append(row)
+            else:
+                # There is an existing version. Check if the attributes
+                # are identical
+                dim_hash = self.keycache[searchtuple]
+                current_hash = self._hash(row)
+                if dim_hash != current_hash:
+                    # The hash of the rows are different. Add the new version
+                    # of the row to the `modified_rows` list, and the id to the
+                    # `obsolete_ids` list.
+                    modified_rows.append(row)
+                    self.obsolete_ids.append(self.idcache[searchtuple])
+        
+                # Add this key to the `seenkeys` dictionary
+                self.seenkeys[searchtuple] = self.idcache[searchtuple]
+
+            if len(new_rows) >= self.chunksize:
+                self._logger.debug('Writing {} rows'.format(len(new_rows)))
+                self._insert(conn, new_rows, self.table_bound)
+                new_rows = list()
+
+            if len(modified_rows) >= self.chunksize:
+                self._logger.debug('Updating {} rows'.format(len(modified_rows)))
+                self._insert(conn, modified_rows, self.table_bound)
+                modified_rows = list()
+        
+        # Insert new rows
+        if new_rows:
+            self._logger.debug('Writing {} rows'.format(len(new_rows)))
+            self._insert(conn, new_rows, self.table_bound)
+        
+        # Insert a new row version of modified rows
+        if modified_rows:
+            self._logger.debug('Updating {} rows'.format(len(modified_rows)))
+            self._insert(conn, modified_rows, self.table_bound)
+
+    def _insert(self, conn, ins_rows, table_bound):
+        """This method does the actual insertion of the rows of data given by
+        ins_rows into the database.
+
+        Args:
+            conn (sqlalchemy.engine): The sqlalchemy connection object.
+            ins_rows (dict): The dictionary of rows with the keys in the
+                format _<column_name>. For example if you have a table with a
+                column name "property", then the key in the dictionary would
+                be "_property". This format is consistent with the bindparam
+                usage in sqlalchemy.
+            table_bound (sqlalchemy.Table): The object referring to the table
+
+        """
+        bound_cols = dict((c, sqlalchemy.bindparam("_" + c.key))
+                          for c in table_bound.columns if c.key != 'id')
+        sql = table_bound.insert().values(bound_cols)
+        conn.execute(sql, ins_rows)
